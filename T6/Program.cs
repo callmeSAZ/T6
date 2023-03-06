88417434fbdf080c53f134d66b310fe78a49ccc7
callmeSAZ <121518844+callmeSAZ@users.noreply.github.com>
2023-03-06 20:31:56 +0330

    Add project files.

---
+++ T6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //دریافت ورودی و جمع یکان و دهگان
+
+            int x = int.Parse(Console.ReadLine());
+            int y, d;
+
+            y = x % 10;
+            d = x / 10;
+
+            Console.WriteLine("Sum={0}", y+d);
+
+            Console.ReadKey();
+        }
+    }
+}
